2682f54874b69247539b2acf8547b926fb0e884f
Mattias Festin <f35531337@gmail.com>
2014-09-06 10:28:02 +0200

    bugfix

--- src/main.js
+++ src/main.js
@@ -2,14 +2,12 @@
 
 var _		= require('lodash');
 
-module.exports = function (_splitChar, _hm) {
-	//The object that holds all values
-	_hm = _hm || {};
-	_splitChar = _splitChar || '.';
+module.exports = function (splitChar) {
+	splitChar = splitChar || '.';
 
 	var createNamespace = function (hm, str, value) {
 			var level = hm,
-				arr = str.split(_splitChar);
+				arr = str.split(splitChar);
 
 			//Delete the "fake" key
 			delete hm[str];
@@ -32,7 +30,7 @@ module.exports = function (_splitChar, _hm) {
 		},
 		getNamespace = function (hm, str) {
 			var level = hm,
-				arr = str.split(_splitChar);
+				arr = str.split(splitChar);
 
 			//Traverse namespace string
 			_.forEach(arr, function (s, i) {
@@ -44,14 +42,17 @@ module.exports = function (_splitChar, _hm) {
 			return level;
 		},
 		deleteNamespace = function (hm, str) {
-			var arr = str.split(_splitChar),
-				preKey = arr.slice(0, arr.length - 1).join(_splitChar),
+			var arr = str.split(splitChar),
+				preKey = arr.slice(0, arr.length - 1).join(splitChar),
 				postKey = arr.slice(arr.length - 1, arr.length),
 				o = preKey.length ? getNamespace(hm, preKey) : hm;
 
 			//Delete last key in
 			delete o[postKey];
 		};
+
+	//The object that holds all values
+	var _hm = {};
 
 	var fn = function (key, value, force) {
 		force = force || false;
@@ -64,9 +65,6 @@ module.exports = function (_splitChar, _hm) {
 	fn.delete = function (key) {
 		deleteNamespace(_hm, key);
 	};
-	
-	fn._hm = _hm;
-	fn._splitChar = _splitChar;
 
 	return fn;
 };
